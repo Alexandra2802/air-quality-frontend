af5eb1d5690ba51117202de774fc1a806d5ddc2f
Alexandra Balo <alexandrabalo@yahoo.com>
2025-05-18 19:31:56 +0300

    changed layout

--- src/components/CentroidMapView.jsx
+++ src/components/CentroidMapView.jsx
@@ -1,8 +1,9 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MapCenterUpdater from "./MapCenterUpdater";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Fix pentru iconițe Leaflet
+// Iconite Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
@@ -10,26 +11,30 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
 });
 
-export default function MapView({ centroid, name }) {
-  if (!centroid) return <p className="text-red-500">Centroidul nu este disponibil.</p>;
+export default function CentroidMapView({ centroid, name }) {
+  if (!centroid) return null;
 
   const { coordinates } = JSON.parse(centroid);
+  const center = [coordinates[1], coordinates[0]]; // lat, lon
 
   return (
     <div className="w-full px-4 mb-8">
       <MapContainer
-        center={[coordinates[1], coordinates[0]]}
+        center={center}
         zoom={10}
         style={{ width: "100%", height: "500px", borderRadius: "12px" }}
       >
+        <MapCenterUpdater center={center} />
+
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+          attribution='&copy; OpenStreetMap'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[coordinates[1], coordinates[0]]}>
+        <Marker position={center}>
           <Popup>{name}</Popup>
         </Marker>
       </MapContainer>
     </div>
   );
 }
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ import { fetchHeatmapData } from "../services/heatmap";
 import { fetchAnimatedHeatmap } from "../services/animatedHeatmap"
 
 import PollutantTabs from "../components/PollutantTabs";
-import MapView from "../components/MapView";
-import HeatmapLayer from "../components/HeatMapLayer";
-import AnimatedHeatmap from "../components/Animatedheatmap"
+import CentroidMapView from "../components/CentroidMapView";
+import Heatmap from "../components/Heatmap";
+import AnimatedHeatmap from "../components/AnimatedHeatmap"
 
 export default function Home() {
   const [activeId, setActiveId] = useState(1);
@@ -51,8 +51,8 @@ export default function Home() {
   }, [activeId, fromDate, toDate]);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Calitatea aerului în România</h1>
+    <div  className="max-w-screen-xl mx-auto px-6">
+      <h1 className="text-2xl font-bold mb-12 mt-15 text-center">Calitatea aerului în România</h1>
 
       <PollutantTabs activeId={activeId} setActiveId={setActiveId} />
 
@@ -77,41 +77,56 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Centroid */}
+      <div className="card shadow rounded-xl p-6 mb-10 mt-10">
       {pollutantData && (
         <>
-          <h2 className="text-lg font-semibold mb-2">Centroid maxim</h2>
+          <h2 className="text-3xl font-bold mb-4 mt-4">Centrul de poluare</h2>
+          <p className="text-lg mb-8 text-gray-600">Această hartă evidențiază locația geografică în care s-a înregistrat cea mai mare valoare a 
+            poluantului selectat, în intervalul de timp ales. Punctul plasat pe hartă indică centrul 
+            geometric (centroidul) regiunii respective.</p>
           <div className="mb-4">
-            <p><strong>Locație:</strong> {pollutantData.name}</p>
-            <p><strong>Valoare maximă:</strong> {Number(pollutantData.max_value).toFixed(10)}</p>
+            <p className="text-xl"><strong>Locație:</strong> {pollutantData.name}</p>
+            <p className="text-xl"><strong>Valoare maximă:</strong> {Number(pollutantData.max_value).toFixed(10)}</p>
           </div>
-          <MapView
+          <CentroidMapView
             centroid={pollutantData.centroid}
             name={pollutantData.name}
           />
         </>
       )}
+      </div>
+      
+      {/* Heatmap */}
+      <div className="card shadow rounded-xl p-6 mb-10 mt-10">
+        <h2 className="text-3xl font-bold mb-4 mt-4">Heatmap</h2>
+        <p className="text-lg mb-8 text-gray-600">Această hartă evidențiază media concentrațiilor poluantului în perioada selectată pe fiecare regiune.</p>
+        {heatmapData && (
+          <MapContainer
+            center={[45.9432, 24.9668]}
+            zoom={6}
+            style={{ height: "600px", width: "100%", borderRadius: "12px" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Heatmap geojson={heatmapData} />
+          </MapContainer>
+        )}
+      </div>
 
-      <h2 className="text-lg font-semibold mt-8 mb-2">Distribuție pe regiuni (Heatmap)</h2>
-      {heatmapData && (
-        <MapContainer
-          center={[45.9432, 24.9668]}
-          zoom={6}
-          style={{ height: "600px", width: "100%", borderRadius: "12px" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <HeatmapLayer geojson={heatmapData} />
-        </MapContainer>
-      )}
-
-      <h2 className="text-lg font-semibold mt-8 mb-2">Animație: evoluția zilnică</h2>
-      {animatedData ? (
-        <AnimatedHeatmap data={animatedData} />
-      ) : (
-        <p>Se încarcă animația...</p>
-      )}
+      {/* Animated Heatmap */}
+      <div className="card shadow rounded-xl p-6 mb-10 mt-10">
+        <h2 className="text-3xl font-bold mb-4 mt-4">Evoluția zilnică</h2>
+        <p className="text-lg mb-8 text-gray-600">Această hartă animată ilustrează evoluția în timp a poluării pe întreg teritoriul României. Culorile se schimbă dinamic pentru fiecare zi din intervalul selectat, evidențiind modul în care poluarea se deplasează sau se intensifică în anumite regiuni de la o zi la alta.</p>
+        {animatedData ? (
+          <AnimatedHeatmap data={animatedData} />
+        ) : (
+          <p>Se încarcă animația...</p>
+        )}
+      </div>
+      
 
     </div>
 
